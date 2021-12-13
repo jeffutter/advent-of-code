01667217ccc4c9b83ac0139b4de29928f315160d
Jeffery Utter <jeff@jeffutter.com>
2021-12-13 13:17:44 -0600

    Add timing output to execution

--- rust/src/module.rs
+++ rust/src/module.rs
@@ -2,6 +2,14 @@
 macro_rules! generate_main {
     ($($mod_name:ident)*) => {
         use util;
+        use std::time::{Duration, Instant};
+
+        fn measure_time<T, F: Fn() -> T>(func: F) -> (T, Duration) {
+            let start = Instant::now();
+            let res = func();
+            let duration = start.elapsed();
+            (res, duration)
+        }
 
         $(
             use $mod_name;
@@ -13,8 +21,11 @@ macro_rules! generate_main {
               let day_s = stringify!($mod_name).trim_start_matches("day");
               let day = usize::from_str_radix(day_s, 10).unwrap();
 
-              println!("Day{:0>2}-01: {}", day, $mod_name::part1(util::read_input("..", day)));
-              println!("Day{:0>2}-02: {}", day, $mod_name::part2(util::read_input("..", day)));
+              let (res, duration) = measure_time(|| $mod_name::part1(util::read_input("..", day)));
+              println!("Day{:0>2}-01 {: >8}μs:\t{}", day, duration.as_micros(), res);
+
+              let (res, duration) = measure_time(|| $mod_name::part2(util::read_input("..", day)));
+              println!("Day{:0>2}-02 {: >8}μs:\t{}", day, duration.as_micros(), res);
             )*
         }
     };
