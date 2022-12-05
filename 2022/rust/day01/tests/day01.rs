1db2b4c5cf85a01c7a2911e83053a55e25651913
Jeffery Utter <jeff@jeffutter.com>
2022-12-04 20:29:23 -0600

    Fix day 1 and 2 test setup

--- 2022/rust/day01/tests/day01.rs
+++ 2022/rust/day01/tests/day01.rs
@@ -1,9 +1,13 @@
 #[test]
 fn day01p01() {
-    assert_eq!(day01::part1(util::read_input("../..", 2022, 1)), 64929)
+    let input = util::read_input("../..", 2022, 1);
+    let data = day01::parse(&input);
+    assert_eq!(day01::part1(data), 64929)
 }
 
 #[test]
 fn day01p02() {
-    assert_eq!(day01::part2(util::read_input("../..", 2022, 1)), 193697)
+    let input = util::read_input("../..", 2022, 1);
+    let data = day01::parse(&input);
+    assert_eq!(day01::part2(data), 193697)
 }
--- 2022/rust/day02/tests/day02.rs
+++ 2022/rust/day02/tests/day02.rs
@@ -1,9 +1,13 @@
 #[test]
 fn day02p01() {
-    assert_eq!(day02::part1(util::read_input("../..", 2022, 2)), 15691)
+    let input = util::read_input("../..", 2022, 2);
+    let data = day02::parse(&input);
+    assert_eq!(day02::part1(data), 15691)
 }
 
 #[test]
 fn day02p02() {
-    assert_eq!(day02::part2(util::read_input("../..", 2022, 2)), 12989)
+    let input = util::read_input("../..", 2022, 2);
+    let data = day02::parse(&input);
+    assert_eq!(day02::part2(data), 12989)
 }
