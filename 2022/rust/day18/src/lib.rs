2df0d4f3b325d850d51fb3b08bb0f9c341013602
Jeffery Utter <jeff@jeffutter.com>
2022-12-17 22:50:00 -0600

    Scaffold day 18

---
+++ 2022/rust/day18/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn part1(s: &str) -> usize {
+    1
+}
+
+pub fn part2(s: &str) -> usize {
+    1
+}
+
+pub fn parse<'a>(data: &'a str) -> &'a str {
+    data
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+}
---
+++ 2022/rust/day18/tests/day18.rs
@@ -0,0 +1,3 @@
+use util::generate_tests;
+
+generate_tests! { 2022, day18, 18, 0, 0}
