273b16c1152cb242593fde72967c565eee95b20b
Jeffery Utter <jeff@jeffutter.com>
2022-12-22 23:25:53 -0600

    Scaffold day23

---
+++ 2022/rust/day23/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn part1(mut state: State) -> usize {
+    1
+}
+
+pub fn part2(state: State) -> i64 {
+    1
+}
+
+pub fn parse<'a>(data: &'a str) -> State {
+    State::new()
+}
+
+pub struct State {}
+
+impl State {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+}
---
+++ 2022/rust/day23/tests/day23.rs
@@ -0,0 +1,3 @@
+use util::generate_tests;
+
+generate_tests! { 2022, day23, 23, 0, 0}
