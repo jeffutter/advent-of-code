6d17c113edc9363f3a5ddf0da7f7b68574497fe6
Jeffery Utter <jeff@jeffutter.com>
2023-12-08 21:38:01 -0600

    Add sample data tests to scaffolding

--- 2023/rust/day16/src/lib.rs
+++ 2023/rust/day16/src/lib.rs
@@ -16,6 +16,20 @@ pub fn part2<'a>(_input: impl Iterator<Item = &'a str>) -> i32 {
 mod tests {
     use util::generate_test;
 
+    const SAMPLE_INPUT: &str = r#""#;
+
+    #[test]
+    fn test_sample_1() {
+        let data = parse(&SAMPLE_INPUT);
+        assert_eq!(part1(data), 0);
+    }
+
+    #[test]
+    fn test_sample_2() {
+        let data = parse(&SAMPLE_INPUT);
+        assert_eq!(part2(data), 0);
+    }
+
     generate_test! { 2023, 16, 1, 0}
     generate_test! { 2023, 16, 2, 0}
 }
