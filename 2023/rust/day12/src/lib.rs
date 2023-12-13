9cf469ef2dec70802b40a8a122bde529fb9bc239
Jeffery Utter <jeff@jeffutter.com>
2023-12-13 12:51:37 -0600

    Finish day 12

--- 2023/rust/day12/src/lib.rs
+++ 2023/rust/day12/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
 
@@ -39,42 +41,38 @@ impl std::fmt::Debug for Row {
 }
 
 impl Row {
-    fn count_ways(&self) -> (usize, Vec<Vec<Spring>>) {
-        let (count, solutions) = count_ways(&self.springs, &self.groups);
-        // println!(
-        //     "Row: {:?} - {}: \n{}",
-        //     &self,
-        //     count,
-        //     solutions.iter().map(|w| debug_fmt_springs(w)).join("\n")
-        // );
-        (count, solutions)
+    fn count_ways(&self) -> usize {
+        count_ways(&self.springs, &self.groups)
     }
 }
 
-fn count_ways(springs: &[Spring], groups: &[u32]) -> (usize, Vec<Vec<Spring>>) {
-    do_count_ways(springs, groups, false, &[])
+fn count_ways(springs: &[Spring], groups: &[u32]) -> usize {
+    let mut cache = HashMap::new();
+    do_count_ways(springs, groups, false, &mut cache)
 }
 
 fn do_count_ways(
     remaining_springs: &[Spring],
     remaining_groups: &[u32],
     prev_complete: bool,
-    visited: &[Spring],
-) -> (usize, Vec<Vec<Spring>>) {
-    // println!(
-    //     "CountWays: {:?}, {:?}, {:?}",
-    //     debug_fmt_springs(remaining_springs),
-    //     remaining_groups,
-    //     prev_complete,
-    // );
-
+    cache: &mut HashMap<(Vec<Spring>, Vec<u32>, bool), usize>,
+) -> usize {
     // No more groups remaining and no more damaged springs. Done. Success.
     if remaining_groups.len() == 0
         && remaining_springs
             .iter()
             .all(|s| *s == Spring::Operational || *s == Spring::Unknown)
     {
-        return (1, [visited.to_vec()].to_vec());
+        return 1;
+    }
+
+    let cache_key = (
+        remaining_springs.to_vec(),
+        remaining_groups.to_vec(),
+        prev_complete,
+    );
+    if let Some(&x) = cache.get(&cache_key) {
+        return x;
     }
 
     // No more groups remaining springs, no more remaining groups, remaining springs aren't enough
@@ -83,12 +81,14 @@ fn do_count_ways(
         || remaining_groups.len() == 0
         || remaining_springs.len() < (remaining_groups[0] as usize)
     {
-        return (0, Vec::new());
+        cache.insert(cache_key, 0);
+        return 0;
     }
 
     // Just completed a group and next spring is Damaged. Done. Fail.
     if prev_complete && remaining_springs[0] == Spring::Damaged {
-        return (0, Vec::new());
+        cache.insert(cache_key, 0);
+        return 0;
     }
 
     // Next spring is Operational or next spring is Unknown and we just
@@ -96,12 +96,9 @@ fn do_count_ways(
     if remaining_springs[0] == Spring::Operational
         || (prev_complete && remaining_springs[0] == Spring::Unknown)
     {
-        return do_count_ways(
-            &remaining_springs[1..],
-            remaining_groups,
-            false,
-            &[&[Spring::Operational], visited].concat(),
-        );
+        let res = do_count_ways(&remaining_springs[1..], remaining_groups, false, cache);
+        cache.insert(cache_key, res);
+        return res;
     }
 
     // Split off a set of springs matching the size of the next group
@@ -114,43 +111,32 @@ fn do_count_ways(
         .all(|s| *s == Spring::Damaged || *s == Spring::Unknown)
     {
         // Take one path where they were all Damaged, thus completing a group
-        let (count_a, solutions_a) = do_count_ways(
-            rest,
-            &remaining_groups[1..],
-            true,
-            &[&[Spring::Damaged].repeat(group_len), visited].concat(),
-        );
+        let count_a = do_count_ways(rest, &remaining_groups[1..], true, cache);
 
         let (next, rest) = remaining_springs.split_first().unwrap();
 
-        let (count_b, solutions_b) = match next {
+        let count_b = match next {
             Spring::Operational => unreachable!(),
-            Spring::Damaged => (0, Vec::new()),
+            Spring::Damaged => 0,
             // Take another path where the first one was Operational
-            Spring::Unknown => do_count_ways(
-                rest,
-                remaining_groups,
-                false,
-                &[&[Spring::Operational], visited].concat(),
-            ),
+            Spring::Unknown => do_count_ways(rest, remaining_groups, false, cache),
         };
 
         // Combine results of both codepaths
-        return (count_a + count_b, [solutions_a, solutions_b].concat());
+        let res = count_a + count_b;
+        cache.insert(cache_key, res);
+        return res;
     }
 
     // All other cases failed, treat this unknown as operational and skip it
     let (next, rest) = remaining_springs.split_first().unwrap();
-    match next {
+    let res = match next {
         Spring::Operational => unreachable!(),
-        Spring::Damaged => (0, Vec::new()),
-        Spring::Unknown => do_count_ways(
-            rest,
-            remaining_groups,
-            prev_complete,
-            &[&[Spring::Operational], visited].concat(),
-        ),
-    }
+        Spring::Damaged => 0,
+        Spring::Unknown => do_count_ways(rest, remaining_groups, prev_complete, cache),
+    };
+    cache.insert(cache_key, res);
+    return res;
 }
 
 pub struct Rows {
@@ -159,7 +145,7 @@ pub struct Rows {
 
 impl Rows {
     fn count_ways(&self) -> usize {
-        self.rows.par_iter().map(|row| row.count_ways().0).sum()
+        self.rows.par_iter().map(|row| row.count_ways()).sum()
     }
 
     fn unfold(&self) -> Self {
@@ -239,11 +225,7 @@ mod tests {
     fn test_sample_1a() {
         let data = parse("???.### 1,1,3");
         let x = data.rows.first().unwrap();
-        let (count, solutions) = count_ways(&x.springs, &x.groups);
-        println!(
-            "Solutions:\n{}",
-            solutions.iter().map(|w| debug_fmt_springs(w)).join("\n")
-        );
+        let count = count_ways(&x.springs, &x.groups);
         assert_eq!(count, 1);
     }
 
@@ -251,11 +233,7 @@ mod tests {
     fn test_sample_1b() {
         let data = parse(".??..??...?##. 1,1,3");
         let x = data.rows.first().unwrap();
-        let (count, solutions) = count_ways(&x.springs, &x.groups);
-        println!(
-            "Solutions:\n{}",
-            solutions.iter().map(|w| debug_fmt_springs(w)).join("\n")
-        );
+        let count = count_ways(&x.springs, &x.groups);
         assert_eq!(count, 4);
     }
 
@@ -263,11 +241,7 @@ mod tests {
     fn test_sample_1g() {
         let data = parse("?.????##??.?#???. 2,3");
         let x = data.rows.first().unwrap();
-        let (count, solutions) = count_ways(&x.springs, &x.groups);
-        println!(
-            "Solutions:\n{}",
-            solutions.iter().map(|w| debug_fmt_springs(w)).join("\n")
-        );
+        let count = count_ways(&x.springs, &x.groups);
         assert_eq!(count, 2);
     }
 
@@ -275,11 +249,7 @@ mod tests {
     fn test_sample_1h() {
         let data = parse("?#.# 2,1");
         let x = data.rows.first().unwrap();
-        let (count, solutions) = count_ways(&x.springs, &x.groups);
-        println!(
-            "Solutions:\n{}",
-            solutions.iter().map(|w| debug_fmt_springs(w)).join("\n")
-        );
+        let count = count_ways(&x.springs, &x.groups);
         assert_eq!(count, 1);
     }
 
@@ -287,11 +257,7 @@ mod tests {
     fn test_sample_1i() {
         let data = parse("#??# 2");
         let x = data.rows.first().unwrap();
-        let (count, solutions) = count_ways(&x.springs, &x.groups);
-        println!(
-            "Solutions:\n{}",
-            solutions.iter().map(|w| debug_fmt_springs(w)).join("\n")
-        );
+        let count = count_ways(&x.springs, &x.groups);
         assert_eq!(count, 0);
     }
 
@@ -299,11 +265,7 @@ mod tests {
     fn test_sample_1j() {
         let data = parse(".??#?#. 3");
         let x = data.rows.first().unwrap();
-        let (count, solutions) = count_ways(&x.springs, &x.groups);
-        println!(
-            "Solutions:\n{}",
-            solutions.iter().map(|w| debug_fmt_springs(w)).join("\n")
-        );
+        let count = count_ways(&x.springs, &x.groups);
         assert_eq!(count, 1);
     }
 
@@ -311,11 +273,7 @@ mod tests {
     fn test_sample_1k() {
         let data = parse("???# 1");
         let x = data.rows.first().unwrap();
-        let (count, solutions) = count_ways(&x.springs, &x.groups);
-        println!(
-            "Solutions:\n{}",
-            solutions.iter().map(|w| debug_fmt_springs(w)).join("\n")
-        );
+        let count = count_ways(&x.springs, &x.groups);
         assert_eq!(count, 1);
     }
 
@@ -335,11 +293,7 @@ mod tests {
     fn test_sample_2a() {
         let data = parse("???.### 1,1,3").unfold();
         let x = data.rows.first().unwrap();
-        let (count, solutions) = count_ways(&x.springs, &x.groups);
-        println!(
-            "Solutions:\n{}",
-            solutions.iter().map(|w| debug_fmt_springs(w)).join("\n")
-        );
+        let count = count_ways(&x.springs, &x.groups);
         assert_eq!(count, 1);
     }
 
@@ -347,15 +301,10 @@ mod tests {
     fn test_sample_2b() {
         let data = parse(".??..??...?##. 1,1,3").unfold();
         let x = data.rows.first().unwrap();
-        let (count, solutions) = count_ways(&x.springs, &x.groups);
-        println!(
-            "Solutions:\n{}",
-            solutions.iter().map(|w| debug_fmt_springs(w)).join("\n")
-        );
-        assert_eq!(count, solutions.iter().unique().count());
+        let count = count_ways(&x.springs, &x.groups);
         assert_eq!(count, 16384);
     }
 
     generate_test! { 2023, 12, 1, 7344}
-    generate_test! { 2023, 12, 2, 0}
+    generate_test! { 2023, 12, 2, 1088006519007}
 }
