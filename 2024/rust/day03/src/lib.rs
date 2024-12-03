029eddaa24ec46d318864a11b663339da946b33f
Jeffery Utter <jeff@jeffutter.com>
2024-12-02 23:46:29 -0600

    Finish day03

--- 2024/rust/day03/src/lib.rs
+++ 2024/rust/day03/src/lib.rs
@@ -1,23 +1,76 @@
-pub fn parse(data: &str) -> impl Iterator<Item = Vec<i32>> + '_ {
-    data.lines().map(|l| {
-        l.split_whitespace()
-            .map(|n| n.parse::<i32>().unwrap())
-            .collect()
-    })
+use regex::Regex;
+
+type RowType = Op;
+type OutType = i32;
+
+#[derive(Debug)]
+pub enum Op {
+    Do,
+    Dont,
+    Mul(i32, i32),
 }
 
-pub fn part1(_input: impl Iterator<Item = Vec<i32>>) -> usize {
-    1
+pub fn parse(data: &str) -> impl Iterator<Item = RowType> + '_ {
+    let re = Regex::new(
+        r"(?:(?<do>do\(\)?)|(?<dont>don't\(\))|(?<mul>mul\((?<n1>\d{1,3}),(?<n2>\d{1,3})\)))",
+    )
+    .unwrap();
+    re.captures_iter(data)
+        .map(|c| {
+            if c.name("do").is_some() {
+                return Op::Do;
+            }
+            if c.name("dont").is_some() {
+                return Op::Dont;
+            }
+
+            let n1 = c.name("n1").unwrap().as_str().parse().unwrap();
+            let n2 = c.name("n2").unwrap().as_str().parse().unwrap();
+            Op::Mul(n1, n2)
+        })
+        .collect::<Vec<_>>()
+        .into_iter()
 }
 
-pub fn part2(_input: impl Iterator<Item = Vec<i32>>) -> usize {
-    1
+pub fn part1(input: impl Iterator<Item = RowType>) -> OutType {
+    input
+        .map(|op| match op {
+            Op::Do => 0,
+            Op::Dont => 0,
+            Op::Mul(a, b) => a * b,
+        })
+        .sum()
+}
+
+pub fn part2(input: impl Iterator<Item = RowType>) -> OutType {
+    input
+        .fold((true, 0), |(emit, acc), op| match (emit, op) {
+            (_, Op::Do) => (true, acc),
+            (_, Op::Dont) => (false, acc),
+            (true, Op::Mul(a, b)) => (true, (a * b) + acc),
+            (false, _) => (false, acc),
+        })
+        .1
 }
 
 #[cfg(test)]
 mod tests {
     use util::generate_test;
 
-    generate_test! { 2024, 3, 1, 0}
-    generate_test! { 2024, 3, 2, 0}
+    #[test]
+    fn sample1() {
+        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        let data = parse(input);
+        assert_eq!(part1(data), 161);
+    }
+
+    #[test]
+    fn sample2() {
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        let data = parse(input);
+        assert_eq!(part2(data), 48);
+    }
+
+    generate_test! { 2024, 3, 1, 161289189}
+    generate_test! { 2024, 3, 2, 83595109}
 }
