89b9a0404892e6fdc997412a2b360003a48e9027
Jeffery Utter <jeff@jeffutter.com>
2024-12-05 08:48:03 -0600

    Add daily template 'make next'

---
+++ rust-template/src/lib.rs
@@ -0,0 +1,29 @@
+type InputType = usize;
+type OutType = usize;
+
+#[allow(unused_variables)]
+pub fn parse(data: &str) -> InputType {
+    1
+}
+
+#[allow(unused_variables)]
+pub fn part1(input: InputType) -> OutType {
+    input
+}
+
+#[allow(unused_variables)]
+pub fn part2(input: InputType) -> OutType {
+    input
+}
+
+#[cfg(test)]
+mod tests {
+    use util::generate_test;
+
+    generate_test!(r#""#, 1, 0);
+
+    generate_test!(r#""#, 2, 0);
+
+    generate_test! { {{year}}, {{day}}, 1, 0}
+    generate_test! { {{year}}, {{day}}, 2, 0}
+}
