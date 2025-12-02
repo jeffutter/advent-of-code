90dd94c4400c30942c8ec9aea72d3c18494c5b61
Jeffery Utter <jeff@jeffutter.com>
2025-12-02 09:16:44 -0600

    Switch nom for winnow as a parser

--- 2025/rust/parser/src/lib.rs
+++ 2025/rust/parser/src/lib.rs
@@ -1,13 +1,11 @@
 use std::{fmt::Display, str::FromStr};
 
-use nom::{
-    branch::alt,
-    bytes::complete::tag,
-    character::complete::{char, digit1},
-    combinator::{map_res, opt, recognize},
-    multi::many1,
-    multi::separated_list1,
-    IResult, Parser,
+use winnow::{
+    ascii::digit1,
+    combinator::{alt, opt, repeat, separated},
+    error::Result,
+    token::literal,
+    Parser,
 };
 use num_traits::Signed;
 use util::Pos;
@@ -15,98 +13,116 @@ use util::Pos;
 pub trait FromDig {
     type Num;
 
-    fn from_dig(s: &str) -> IResult<&str, Self::Num>;
+    fn from_dig<'a>(input: &mut &'a str) -> Result<Self::Num>;
 }
 
 impl FromDig for u32 {
     type Num = u32;
-    fn from_dig(s: &str) -> IResult<&str, Self::Num> {
-        map_res(digit1, |s: &str| s.parse::<u32>()).parse(s)
+    fn from_dig<'a>(input: &mut &'a str) -> Result<Self::Num> {
+        digit1.try_map(|s: &str| s.parse::<u32>()).parse_next(input)
     }
 }
 
 impl FromDig for i32 {
     type Num = i32;
-    fn from_dig(s: &str) -> IResult<&str, Self::Num> {
-        map_res(digit1, |s: &str| s.parse::<i32>()).parse(s)
+    fn from_dig<'a>(input: &mut &'a str) -> Result<Self::Num> {
+        digit1.try_map(|s: &str| s.parse::<i32>()).parse_next(input)
     }
 }
 
 impl FromDig for i64 {
     type Num = i64;
-    fn from_dig(s: &str) -> IResult<&str, Self::Num> {
-        map_res(digit1, |s: &str| s.parse::<i64>()).parse(s)
+    fn from_dig<'a>(input: &mut &'a str) -> Result<Self::Num> {
+        digit1.try_map(|s: &str| s.parse::<i64>()).parse_next(input)
     }
 }
 
 impl FromDig for i128 {
     type Num = i128;
-    fn from_dig(s: &str) -> IResult<&str, Self::Num> {
-        map_res(digit1, |s: &str| s.parse::<i128>()).parse(s)
+    fn from_dig<'a>(input: &mut &'a str) -> Result<Self::Num> {
+        digit1.try_map(|s: &str| s.parse::<i128>()).parse_next(input)
     }
 }
 
 impl FromDig for usize {
     type Num = usize;
-    fn from_dig(s: &str) -> IResult<&str, Self::Num> {
-        map_res(digit1, |s: &str| s.parse::<usize>()).parse(s)
+    fn from_dig<'a>(input: &mut &'a str) -> Result<Self::Num> {
+        digit1.try_map(|s: &str| s.parse::<usize>()).parse_next(input)
     }
 }
 
-pub fn point<'a, T>(sep: &str) -> impl Fn(&'a str) -> IResult<&'a str, Pos<T>> + '_
+pub fn point<'a, T>(sep: &str) -> impl FnMut(&mut &'a str) -> Result<Pos<T>> + '_
 where
     T: Display + FromDig<Num = T>,
 {
-    move |s: &'a str| {
-        let (rest, (l, _, r)) =
-            (<T as FromDig>::from_dig, tag(sep), <T as FromDig>::from_dig).parse(s)?;
-        Ok((rest, Pos::new(l, r)))
+    move |input: &mut &'a str| {
+        let l = <T as FromDig>::from_dig(input)?;
+        literal(sep).parse_next(input)?;
+        let r = <T as FromDig>::from_dig(input)?;
+        Ok(Pos::new(l, r))
     }
 }
 
-pub fn signed_point<'a, T>(sep: &str) -> impl Fn(&'a str) -> IResult<&'a str, Pos<T>> + '_
+pub fn signed_point<'a, T>(sep: &str) -> impl FnMut(&mut &'a str) -> Result<Pos<T>> + '_
 where
     T: Display + Signed + FromStr,
+    <T as FromStr>::Err: std::error::Error + Send + Sync + 'static,
 {
-    move |s: &'a str| {
-        let (rest, (l, _, r)) = (signed_dig, tag(sep), signed_dig).parse(s)?;
-        Ok((rest, Pos::new(l, r)))
+    move |input: &mut &'a str| {
+        let l = signed_dig(input)?;
+        literal(sep).parse_next(input)?;
+        let r = signed_dig(input)?;
+        Ok(Pos::new(l, r))
     }
 }
 
-pub fn dig_pair<'a, T>(sep: &str) -> impl Fn(&'a str) -> IResult<&'a str, (T, T)> + '_
+pub fn dig_pair<'a, T>(sep: &str) -> impl FnMut(&mut &'a str) -> Result<(T, T)> + '_
 where
     T: Display + FromDig<Num = T>,
 {
-    move |s: &'a str| {
-        let (rest, (l, _, r)) =
-            (<T as FromDig>::from_dig, tag(sep), <T as FromDig>::from_dig).parse(s)?;
-        Ok((rest, (l, r)))
+    move |input: &mut &'a str| {
+        let l = <T as FromDig>::from_dig(input)?;
+        literal(sep).parse_next(input)?;
+        let r = <T as FromDig>::from_dig(input)?;
+        Ok((l, r))
     }
 }
 
-pub fn signed_dig_pair<'a, T>(sep: &str) -> impl Fn(&'a str) -> IResult<&'a str, (T, T)> + '_
+pub fn signed_dig_pair<'a, T>(sep: &str) -> impl FnMut(&mut &'a str) -> Result<(T, T)> + '_
 where
     T: Display + Signed + FromStr,
+    <T as FromStr>::Err: std::error::Error + Send + Sync + 'static,
 {
-    move |s: &'a str| {
-        let (rest, (l, _, r)) = (signed_dig, tag(sep), signed_dig).parse(s)?;
-        Ok((rest, (l, r)))
+    move |input: &mut &'a str| {
+        let l = signed_dig(input)?;
+        literal(sep).parse_next(input)?;
+        let r = signed_dig(input)?;
+        Ok((l, r))
     }
 }
 
-pub fn separated_digits<'a, T>(sep: &str) -> impl Fn(&'a str) -> IResult<&'a str, Vec<T>> + '_
+pub fn separated_digits<'a, T>(sep: &str) -> impl FnMut(&mut &'a str) -> Result<Vec<T>> + '_
 where
     T: Display + FromDig<Num = T>,
 {
-    move |s: &'a str| separated_list1(many1(alt((tag(sep), tag(" ")))), <T as FromDig>::from_dig).parse(s)
+    let sep_str = sep.to_string();
+    move |input: &mut &'a str| {
+        separated(
+            1..,
+            |i: &mut &'a str| <T as FromDig>::from_dig(i),
+            repeat::<_, _, String, _, _>(1.., alt((literal(sep_str.as_str()), literal(" ")))),
+        )
+        .parse_next(input)
+    }
 }
 
-pub fn signed_dig<T>(s: &str) -> IResult<&str, T>
+pub fn signed_dig<T>(input: &mut &str) -> Result<T>
 where
     T: Signed + FromStr,
+    <T as FromStr>::Err: std::error::Error + Send + Sync + 'static,
 {
-    map_res(recognize((opt(char('-')), digit1)), |s: &str| {
-        s.parse::<T>()
-    }).parse(s)
+    (opt('-'), digit1)
+        .take()
+        .try_map(|s: &str| s.parse::<T>())
+        .parse_next(input)
 }
